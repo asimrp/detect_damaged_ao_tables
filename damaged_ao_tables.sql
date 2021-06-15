0cb247cd443fe80f533c1f5b38f066c61c668161
Asim R P <apraveen@pivotal.io>
2021-06-15 18:37:14 +0530

    SQL script to detect damaged append-optimized tables
    In one Greenplum 5X environment, it was found that an aoseg table's
    relfilenode on primary was empty but its copy on the mirror was not
    empty.  And the primary and mirror pair was in-sync.  Persistent
    tables - gp_relation_node and gp_persistent_relation_node and the
    filesystem on primary indicated that the AO table had one or more
    segment files on disk.  Select on the AO table was failing with
    "gp_relation_node has more entries than ..." error.
    
    After copying the aoseg table's relfilenode from mirror into primary
    data directory, the error was gone and select started working.
    
    This SQL script is an attempt to detect such damaged AO tables by
    comparing contents of aoseg and gp_relation_node for all
    append-optimized tables in the system.  Knowing the extent of the
    damage is the first step towards fixing it.
    
    Note that before running the SQL script, it is assumed that gpcheckcat
    has reported no catalog issues.

---
+++ damaged_ao_tables.sql
@@ -0,0 +1,72 @@
+--
+-- This SQL script determines damaged append-optimized tables.  The
+-- damage is defined as discrepancy in segment file information
+-- between persistent table and aoseg auxiliary table.  The script
+-- outputs names of damaged append-optimized table and its aoseg
+-- table.  The user is expected to manually fix the problem by first
+-- executing a "select count(*) <tablename>" on each damaged
+-- append-optimized table.  The select should fail with an error
+-- claiming discrepancy between gp_relation_node and aoseg table.
+-- Next step is to check if the aoseg relation can be fixed by
+-- bringing a copy of its relfilenode from the affected segment's
+-- mirror.
+--
+-- Assumption: "gpcheckcat" has passed all the checks on this
+-- database.
+
+begin;
+
+-- Use the same snapshot to scan persistent table and auxiliary tables
+-- so that concurrent DDL operations do not affect the results.
+set transaction isolation level serializable;
+
+-- Create a table with one row for each column oriented table
+-- containing unique segment numbers recorded in persistent table.
+drop table if exists pt_ao_column_segfile_count;
+create table pt_ao_column_segfile_count as
+  (select g.gp_segment_id as segid, a.relid, a.segrelid, count(g.segment_file_num) as pt_count
+   from gp_dist_random('gp_relation_node') g, pg_class c, pg_appendonly a
+   where g.relfilenode_oid = c.relfilenode and c.oid = a.relid and a.columnstore = true
+   and g.segment_file_num >=0 and g.segment_file_num < 128
+   group by g.gp_segment_id, a.relid, a.segrelid, c.relname)
+  distributed by (segid);
+
+-- Create a table with one row for each aocsseg auxiliary table
+-- containing the count of segment numbers.
+drop table if exists aocsseg_count;
+create table aocsseg_count as
+  (select a.gp_segment_id as segid, a.relid, a.segrelid,
+  	  (select count(segno) from gp_toolkit.__gp_aocsseg(a.relid)
+	   where physical_segno >= 0 and physical_segno < 128) as aoseg_count
+   from gp_dist_random('pg_appendonly') a
+   where columnstore=true)
+  distributed by (segid);
+
+-- Populate the same information for row-oriented append-optimized
+-- tables.
+insert into pt_ao_column_segfile_count
+  (select g.gp_segment_id as segid, a.relid::regclass, a.segrelid, count(g.segment_file_num) as pt_count
+   from gp_dist_random('gp_relation_node') g, pg_class c, pg_appendonly a
+   where g.relfilenode_oid = c.relfilenode and c.oid = a.relid and a.columnstore = false
+   group by g.gp_segment_id, a.relid, a.segrelid, c.relname);
+
+insert into aocsseg_count
+  (select a.gp_segment_id as segid, a.relid, a.segrelid,
+  	  (select count(segno) from
+	   gp_toolkit.__gp_aoseg_name(a.relid::regclass::text))
+	  as aoseg_count
+   from gp_dist_random('pg_appendonly') a
+   where columnstore=false);
+
+-- Information between gp_relation_node and aoseg auxiliary can differ
+-- at the most by 1.  This is because an empty table, right after
+-- creation, contains one entry in gp_relation_node but its aoseg
+-- table is empty.  The following query identifies tables having
+-- discrepancy segment file counts recorded in aoseg vs persistent
+-- table.
+select p.segid, a.relid::regclass, a.segrelid::regclass, a.aoseg_count, p.pt_count
+  from aocsseg_count a full outer join pt_ao_column_segfile_count p
+  on a.segid = p.segid and a.relid = p.relid
+  where p.pt_count is null or a.aoseg_count is null or p.pt_count - a.aoseg_count > 1;
+
+end;
