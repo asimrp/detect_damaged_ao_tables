4b000d81cb17815908da167ca6bcc916c2e90a59
Asim R P <apraveen@pivotal.io>
2021-06-18 18:24:40 +0530

    Use better table names, indentation and store results in a table
    The damaged table details are stored in a tale named damaged_ao_tables
    first and then queried from it at the end.

--- damaged_ao_tables.sql
+++ damaged_ao_tables.sql
@@ -20,43 +20,82 @@ begin;
 -- so that concurrent DDL operations do not affect the results.
 set transaction isolation level serializable;
 
+\timing on
+
 -- Create a table with one row for each column oriented table
 -- containing unique segment numbers recorded in persistent table.
-drop table if exists pt_ao_column_segfile_count;
-create table pt_ao_column_segfile_count as
-  (select g.gp_segment_id as segid, a.relid, a.segrelid, count(g.segment_file_num) as pt_count
-   from gp_dist_random('gp_relation_node') g, gp_dist_random('pg_class') c, pg_appendonly a
-   where g.gp_segment_id = c.gp_segment_id and g.relfilenode_oid = c.relfilenode
-   and c.oid = a.relid and a.columnstore = true
-   and g.segment_file_num >=0 and g.segment_file_num < 128
+drop table if exists pt_ao_segfile_count;
+create table pt_ao_segfile_count as
+  (select
+       g.gp_segment_id as segid,
+       a.relid,
+       a.segrelid,
+       count(g.segment_file_num) as pt_count
+   from
+       gp_dist_random('gp_relation_node') g,
+       gp_dist_random('pg_class') c,
+       pg_appendonly a
+   where
+       g.gp_segment_id = c.gp_segment_id
+       and g.relfilenode_oid = c.relfilenode
+       and c.oid = a.relid and a.columnstore = true
+       and g.segment_file_num >=0 and g.segment_file_num < 128
    group by g.gp_segment_id, a.relid, a.segrelid, c.relname)
   distributed by (segid);
 
 -- Create a table with one row for each aocsseg auxiliary table
 -- containing the count of segment numbers.
-drop table if exists aocsseg_count;
-create table aocsseg_count as
-  (select a.gp_segment_id as segid, a.relid, a.segrelid,
-  	  (select count(segno) from gp_toolkit.__gp_aocsseg(a.relid)
-	   where physical_segno >= 0 and physical_segno < 128) as aoseg_count
+drop table if exists aux_ao_segfile_count;
+create table aux_ao_segfile_count as
+  (select
+       a.gp_segment_id as segid,
+       a.relid,
+       a.segrelid,
+       (select
+            count(segno)
+        from
+	    gp_toolkit.__gp_aocsseg(a.relid)
+	where
+	    physical_segno >= 0
+	    and physical_segno < 128)
+       as aoseg_count
    from gp_dist_random('pg_appendonly') a
    where columnstore=true)
   distributed by (segid);
 
 -- Populate the same information for row-oriented append-optimized
 -- tables.
-insert into pt_ao_column_segfile_count
-  (select g.gp_segment_id as segid, a.relid::regclass, a.segrelid, count(g.segment_file_num) as pt_count
-   from gp_dist_random('gp_relation_node') g, gp_dist_random('pg_class') c, pg_appendonly a
-   where g.gp_segment_id = c.gp_segment_id and g.relfilenode_oid = c.relfilenode
-   and c.oid = a.relid and a.columnstore = false
-   group by g.gp_segment_id, a.relid, a.segrelid, c.relname);
+insert into pt_ao_segfile_count
+  (select
+       g.gp_segment_id as segid,
+       a.relid,
+       a.segrelid,
+       count(g.segment_file_num) as pt_count
+   from
+       gp_dist_random('gp_relation_node') g,
+       gp_dist_random('pg_class') c,
+       pg_appendonly a
+   where
+       g.gp_segment_id = c.gp_segment_id
+       and g.relfilenode_oid = c.relfilenode
+       and c.oid = a.relid
+       and a.columnstore = false
+   group by
+       g.gp_segment_id,
+       a.relid,
+       a.segrelid,
+       c.relname);
 
-insert into aocsseg_count
-  (select a.gp_segment_id as segid, a.relid, a.segrelid,
-  	  (select count(segno) from
-	   gp_toolkit.__gp_aoseg_name(a.relid::regclass::text))
-	  as aoseg_count
+insert into aux_ao_segfile_count
+  (select
+       a.gp_segment_id as segid,
+       a.relid,
+       a.segrelid,
+       (select
+            count(segno)
+        from
+	    gp_toolkit.__gp_aoseg_name(a.relid::regclass::text))
+       as aoseg_count
    from gp_dist_random('pg_appendonly') a
    where columnstore=false);
 
@@ -64,11 +103,38 @@ insert into aocsseg_count
 -- at the most by 1.  This is because an empty table, right after
 -- creation, contains one entry in gp_relation_node but its aoseg
 -- table is empty.  The following query identifies tables having
--- discrepancy segment file counts recorded in aoseg vs persistent
--- table.
-select p.segid, a.relid::regclass, a.segrelid::regclass, a.aoseg_count, p.pt_count
-  from aocsseg_count a full outer join pt_ao_column_segfile_count p
-  on a.segid = p.segid and a.relid = p.relid
-  where p.pt_count is null or a.aoseg_count is null or p.pt_count - a.aoseg_count > 1;
+-- discrepancy between segment file counts recorded in aoseg vs
+-- persistent table.
+drop table if exists damaged_ao_tables;
+create table damaged_ao_tables as
+  (select
+       p.segid as gpsegid,
+       a.relid as ao_oid,
+       a.segrelid as aoseg_oid,
+       a.aoseg_count,
+       p.pt_count
+   from
+       aux_ao_segfile_count a
+       full outer join
+       pt_ao_segfile_count p
+       on a.segid = p.segid
+       and a.relid = p.relid
+   where
+       p.pt_count is null
+       or a.aoseg_count is null
+       or p.pt_count - a.aoseg_count > 1)
+  distributed by(gpsegid);
 
 end;
+
+--
+-- Final output: damaged table names and their aoseg table OIDs
+--
+select
+    gpsegid,
+    ao_oid::regclass,
+    aoseg_oid,
+    aoseg_count,
+    pt_count
+from
+    damaged_ao_tables;
